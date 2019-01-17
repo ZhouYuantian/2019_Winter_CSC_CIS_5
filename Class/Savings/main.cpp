84447f1d1f548a2a966b88555c3c768da97b18b7
ml1150258 <mark.lehr@rcc.edu>
2019-01-17 12:08:32 -0800

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

--- Class/Savings/main.cpp
+++ Class/Savings/main.cpp
@@ -17,10 +17,13 @@ using namespace std;
 const float PERCENT=100.0f;//Percent conversion
 
 //Function Prototypes
-float save1(float,float,int);
-float save2(float,float,int);
-float save3(float,float,int);
-float save4(float,float,int);
+float save1(float,float,int);//Using Power Function
+float save2(float,float,int);//Using Log and Exponential
+float save3(float,float,int);//For-Loop
+float save4(float,float,int);//Recursion
+int   save5(float &,float,int);//Reference Parameter and Static Variable
+float save6(float,float,int = 12);//Defaulted Parameter
+float save4(float,float,float);//Using same function name/Overloading
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
@@ -36,13 +39,47 @@ int main(int argc, char** argv) {
     
     //Map inputs -> outputs
     iR/=PERCENT;
-    cout<<"Savings Function using pow        = $"<<save1(pv,iR,n)<<endl;
-    cout<<"Savings Function using exp/log    = $"<<save2(pv,iR,n)<<endl;
-    cout<<"Savings Function using for - loop = $"<<save3(pv,iR,n)<<endl;
-    cout<<"Savings Function using itself     = $"<<save4(pv,iR,n)<<endl;
+    cout<<"Savings Function using pow         = $"<<save1(pv,iR,n)<<endl;
+    cout<<"Savings Function using exp/log     = $"<<save2(pv,iR,n)<<endl;
+    cout<<"Savings Function using for - loop  = $"<<save3(pv,iR,n)<<endl;
+    cout<<"Savings Function using itself      = $"<<save4(pv,iR,n)<<endl;
+    float pvs;
+    for(int i=1;i<=4;i++){
+        pvs=pv;
+        save5(pvs,iR,n);
+    }
+    pvs=pv;
+    int nClld=save5(pvs,iR,n);
+    cout<<"Savings 5 using static variable and a reference parameter was called "
+        <<nClld<<" times"<<endl;
+    cout<<"Savings Function using reference   = $"<<pvs<<endl;
+    cout<<"Savings Function using default     = $"<<save6(pv,iR)<<endl;
+    cout<<"Savings Function using overloading = $"
+            <<save4(pv,iR,static_cast<float>(n))<<endl;
+    
 
     //Exit stage right or left!
     return 0;
+}
+
+float save4(float pv,float i,float n){
+    if(n<=0)return pv;
+    return save4(pv,i,n-1)*(1+i);
+}
+
+float save6(float pv,float i,int n){
+    for(int nc=1;nc<=n;nc++){
+        pv*=(1+i);
+    }
+    return pv;
+}
+
+int   save5(float &pv,float i,int n){
+    static int count=0;
+    for(int nc=1;nc<=n;nc++){
+        pv*=(1+i);
+    }
+    return ++count;
 }
 
 float save4(float pv,float i,int n){
